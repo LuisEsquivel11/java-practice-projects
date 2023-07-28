6c92ba9e1fa84bb28990cd75d27f3d11683c445f
Luis <LuisDEsquivel11@gmail.com>
2023-07-27 20:11:03 -0500

    salary calculatorif statement

---
+++ src/decision_structure/SalaryCalculator.java
@@ -0,0 +1,25 @@
+package decision_structure;
+
+import java.util.Scanner;
+
+public class SalaryCalculator {
+
+    public static void main(String[] args) {
+/* All sales people get a payment of $1000 for the week.
+        Sales people who exceed 10 sales get an additional bonus of $250 */
+
+        double salary = 1000;
+        double bonus = 250;
+        int quota = 10;
+
+        System.out.println("How many sales did the employee get this week?");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+
+        if(sales > quota) {
+            salary = salary + bonus;
+        }
+
+        System.out.println("Salary: " + salary);
+    }
+}
