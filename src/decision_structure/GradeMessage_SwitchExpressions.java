6d07aa50a33a25b22ff549ea15e1a55ac8b17e44
Luis <LuisDEsquivel11@gmail.com>
2023-07-29 15:42:11 -0500

    Switch expressions grade message

---
+++ src/decision_structure/GradeMessage_SwitchExpressions.java
@@ -0,0 +1,25 @@
+package decision_structure;
+
+import java.util.Scanner;
+
+public class GradeMessage_SwitchExpressions {
+
+    public static void main(String[] args) {
+        System.out.println("Enter your letter grade");
+        Scanner scanner = new Scanner(System.in);
+        String grade = scanner.next();
+        scanner.close();
+
+
+        String message = switch(grade) {
+            case "A" -> "Excellent job!";
+            case "B", "B+" -> "Great Job!";
+            case "C" -> "Good Job!";
+            case "D" ->  "You need to do better";
+            case "F" -> "Uh oh!";
+            default ->  "Error. Invalid grade";
+        };
+
+        System.out.println(message);
+    }
+}
